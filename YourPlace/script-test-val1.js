ed6f151b100e41a99deebc13b2f4dafc6ba2db9d
Elit0o <eli8.2013@gmail.com>
2023-12-27 14:33:57 +0200

    questions and redirections to the main page

---
+++ YourPlace/script-test-val1.js
@@ -0,0 +1,4 @@
+document.getElementById('answer1').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'test.html';
+});
---
+++ YourPlace/script-test-val2.js
@@ -0,0 +1,4 @@
+document.getElementById('answer2').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'main-page.html';
+});
